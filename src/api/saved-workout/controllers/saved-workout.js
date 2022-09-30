1d43fb432a2676127174401de4575d6d472a91e8
Vishesh Dubey <visheshdubey2016@gmail.com>
2022-09-30 21:26:37 +0530

    second

---
+++ src/api/saved-workout/controllers/saved-workout.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * saved-workout controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::saved-workout.saved-workout');
---
+++ src/api/saved-workout/routes/saved-workout.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * saved-workout router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::saved-workout.saved-workout');
