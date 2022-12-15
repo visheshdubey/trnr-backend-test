c9a2c65b8bfd0088924cb8238496bb0469393219
Ubuntu <ubuntu@ip-172-31-19-252.ap-southeast-2.compute.internal>
2022-12-15 08:45:46 +0000

    Merge

---
+++ config/plugins.js
@@ -0,0 +1,44 @@
+module.exports = ({ env }) => (
+    {
+        upload: {
+            config: {
+                provider: 'aws-s3',
+                providerOptions: {
+                    accessKeyId: env('AWS_ACCESS_KEY_ID'),
+                    secretAccessKey: env('AWS_ACCESS_SECRET'),
+                    region: env('AWS_REGION'),
+                    params: {
+                        Bucket: env('AWS_BUCKET_NAME'),
+                        defaultContainerName: env('STORAGE_CONTAINER_NAME')
+                    },
+                },
+                // These parameters could solve issues with ACL public-read access — see [this issue](https://github.com/strapi/strapi/issues/5868) for details
+                actionOptions: {
+                    upload: {
+                        ACL: null
+                    },
+                    uploadStream: {
+                        ACL: null
+                    },
+                }
+            },
+        },
+        email: {
+            config: {
+                provider: 'nodemailer',
+                providerOptions: {
+                    host: env('SMTP_HOST'),
+                    port: env('SMTP_PORT'),
+                    auth: {
+                        user: env('SMTP_USERNAME'),
+                        pass: env('SMTP_PASSWORD'),
+                    },
+                    // ... any custom nodemailer options
+                },
+                settings: {
+                    defaultFrom: 'trnr@stackwaystech.com',
+                    defaultReplyTo: 'trnr@stackwaystech.com',
+                },
+            },
+        },
+    });
