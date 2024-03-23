fcbc14cbd4ff2dad2a71c5a5153d2af137565727
Vishesh Dubey <visheshdubey2016@gmail.com>
2024-03-23 12:04:15 +0530

    Updated Strapi latest version

--- config/database.js
+++ config/database.js
@@ -1,11 +1,30 @@
-const path = require('path');
+// const path = require('path');
 
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: 'sqlite',
+//     connection: {
+//       filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+//     },
+//     useNullAsDefault: true,
+//   },
+// });
+// const parse = require('pg-connection-string').parse;
+// const config = parse(process.env.DATABASE_URL);
 module.exports = ({ env }) => ({
   connection: {
-    client: 'sqlite',
+    client: 'postgres',
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      host: process.env.DATABASE_HOST,//config.host,
+      port: process.env.DATABASE_PORT,//config.port,
+      database: process.env.DATABASE_NAME,//config.database,
+      user: process.env.DATABASE_USERNAME,//config.user,
+      password: process.env.DATABASE_PASSWORD,//config.password,
+      // ssl: {
+      //   rejectUnauthorized: false
+      // },
+      ssl: false
     },
-    useNullAsDefault: true,
+    debug: true,
   },
 });
